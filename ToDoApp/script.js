f82851cd77d75a7956212984aae03a7f3ee85dc6
Kacper <kacpermargol@gmail.com>
2024-06-18 11:11:45 +0200

    first commit

---
+++ ToDoApp/script.js
@@ -0,0 +1,61 @@
+let todoList = [];
+
+// Load Todo List from local storage
+function loadTodoList() {
+	let storedTodoList = localStorage.getItem('todoList');
+	if (storedTodoList) {
+		todoList = JSON.parse(storedTodoList);
+	}
+}
+
+// Save Todo List to local storage
+function saveTodoList() {
+	localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+
+document.getElementById('add-btn').addEventListener('click', addTodo);
+
+function addTodo() {
+	let todoInput = document.getElementById('todo-input');
+	let todoText = todoInput.value.trim();
+
+	if (todoText !== '') {
+		todoList.push({ text: todoText, completed: false });
+		todoInput.value = '';
+		saveTodoList();
+		renderTodoList();
+	}
+}
+
+function renderTodoList() {
+	let todoListHTML = '';
+	todoList.forEach((todo, index) => {
+		todoListHTML += `
+            <li>
+                <input type="checkbox" ${todo.completed ? 'checked' : ''}>
+                <span ${todo.completed ? 'class="completed"' : ''}>${
+			todo.text
+		}</span>
+                <button class="delete-btn" data-index="${index}">X</button>
+            </li>
+        `;
+	});
+	document.getElementById('todo-list').innerHTML = todoListHTML;
+
+	let deleteBtns = document.querySelectorAll('.delete-btn');
+	deleteBtns.forEach((btn) => {
+		btn.addEventListener('click', deleteTodo);
+	});
+}
+
+function deleteTodo(event) {
+	let index = event.target.getAttribute('data-index');
+	todoList.splice(index, 1);
+	saveTodoList();
+	renderTodoList();
+}
+
+loadTodoList();
+renderTodoList();
+
+document.addEventListener('DOMContentLoaded', renderTodoList);
