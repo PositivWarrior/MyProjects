c3ece81e73bf02ed6af5f836b48497dc1f7a3df0
Kacper <kacpermargol@gmail.com>
2024-06-28 12:08:54 +0200

    Blog App v1

---
+++ BlogApp/models/Article.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const marked = require('marked');
+const slugify = require('slugify');
+
+const articleSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+	},
+	markdown: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	slug: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	html: {
+		type: String,
+		default: function () {
+			return marked(this.markdown);
+		},
+	},
+});
+
+module.exports = mongoose.model('Article', articleSchema);
